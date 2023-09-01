a7e2fe10e611dac45c895a5b5e43035220dea427
Piotr Migdal <pmigdal@gmail.com>
2023-09-01 15:26:59 +0200

    animation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Dance from "./components/Dance.vue";
 </script>
 
 <template>
@@ -11,6 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <Dance />
   <HelloWorld msg="Vite + Vue" />
 </template>
 
---
+++ src/components/Dance.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted, computed } from "vue";
+
+const timeStepMs = 10;
+const timeElapsed = ref(0); // in seconds
+
+let interval: number;
+
+onMounted(() => {
+  interval = setInterval(() => {
+    timeElapsed.value += timeStepMs / 1000;
+  }, timeStepMs);
+});
+
+onUnmounted(() => {
+  clearInterval(interval);
+});
+
+const radius = ref(30);
+const x = computed(() => 200 + 100 * Math.sin(3 * timeElapsed.value));
+</script>
+
+<template>
+  <svg width="500" height="500">
+    <circle
+      :cx="x"
+      :cy="200"
+      :r="radius"
+      stroke="black"
+      stroke-width="3"
+      fill="red"
+    />
+  </svg>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
