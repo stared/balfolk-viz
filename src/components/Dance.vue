6d31a5d6ef0785dcff8d77abda63fd3efdc146ac
Piotr Migdal <pmigdal@gmail.com>
2023-09-21 19:02:07 +0200

    followers and leaders - branle de noirmoutier v1

--- src/components/Dance.vue
+++ src/components/Dance.vue
@@ -28,28 +28,51 @@ const branleX = periodic([
 
 const branleR = (x: number) => Math.cos(2 * Math.PI * x);
 
-const branleAnglePart2 = periodic([
+const branleAnglePart2Leader = periodic([
   (_x) => 0,
   (x) => -180 * easeCubicInOut(x),
   (_x) => -180,
   (x) => -180 * (1 + easeCubicInOut(x)),
 ]);
 
-const branleAngle = periodic([
+const branleAngleLeader = periodic([
   (_x) => 0,
   (_x) => 0,
-  (x) => branleAnglePart2(4 * x),
-  (x) => branleAnglePart2(4 * x),
+  (x) => branleAnglePart2Leader(4 * x),
+  (x) => branleAnglePart2Leader(4 * x),
 ]);
 
 const x = computed(() => 300 + 200 * branleX(timeElapsed.value));
 const radius = computed(() => 20 + 1 * branleR(timeElapsed.value));
-const rotation = computed(() => 90 + branleAngle(timeElapsed.value / 4));
+const rotationLeader = computed(
+  () => 90 + branleAngleLeader(timeElapsed.value / 4)
+);
+const rotationFollower = computed(
+  () => 270 + branleAngleLeader(timeElapsed.value / 4 + 3.5)
+);
+
+const n = 10;
+const iterN = Array(n);
 </script>
 
 <template>
   <svg width="800" height="800">
-    <Dancer :x="x" :y="200" :scale="radius" color="red" :rotation="rotation" />
+    <Dancer
+      v-for="(_, i) in iterN"
+      :x="x - 100"
+      :y="100 * i"
+      :scale="radius"
+      color="red"
+      :rotation="rotationLeader"
+    />
+    <Dancer
+      v-for="(_, i) in iterN"
+      :x="x + 100"
+      :y="100 * i"
+      :scale="radius"
+      color="blue"
+      :rotation="rotationFollower"
+    />
   </svg>
 </template>
 
