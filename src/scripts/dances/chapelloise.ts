feb3d0caf62138f8b0c22c98546bf8cb5f1f10b9
Piotr Migdal <pmigdal@gmail.com>
2025-01-26 19:36:53 +0100

    chapelloise partial fix

--- src/scripts/dances/chapelloise.ts
+++ src/scripts/dances/chapelloise.ts
@@ -5,9 +5,7 @@ export const chapelloise = (nPairs: number): Dance => {
   const dance = Dance.empty();
 
   // we need something position-based for any dances with swapping
-  const firstLeader = Positions.new(
-    DancerPosition.new({ x: -0.5, y: 5, r: 0, angle: 0 })
-  )
+  const firstLeader = Positions.new(DancerPosition.noShift())
     .step({ y: -1 })
     .step({ y: -1 })
     .step({ y: -1, angle: 180 })
@@ -16,17 +14,15 @@ export const chapelloise = (nPairs: number): Dance => {
     .step({ y: 1 })
     .step({ y: 1, angle: 180 })
     .step({ y: 1 })
-    .step({ x: 0.5 })
-    .step({ x: -0.5 })
+    .step({ x: 0.3 })
+    .step({ x: -0.3 })
     .step({ x: 1 })
-    .step({ x: -0.5 })
-    .step({ x: 0.5 })
+    .step({ x: -0.3 })
+    .step({ x: 0.3 })
     .step({ x: -1 })
     .toMovements();
 
-  const firstFollower = Positions.new(
-    DancerPosition.new({ x: 0.5, y: 5, r: 0, angle: 0 })
-  )
+  const firstFollower = Positions.new(DancerPosition.noShift())
     .step({ y: -1 })
     .step({ y: -1 })
     .step({ y: -1, angle: 180 })
@@ -35,26 +31,26 @@ export const chapelloise = (nPairs: number): Dance => {
     .step({ y: 1 })
     .step({ y: 1, angle: 180 })
     .step({ y: 1 })
-    .step({ x: +0.5 })
-    .step({ x: 0.5 })
+    .step({ x: -0.3 })
+    .step({ x: 0.3 })
     .step({ x: -1 })
-    .step({ x: 0.5 })
-    .step({ x: +0.5 })
+    .step({ x: 0.3 })
+    .step({ x: -0.3 })
     .step({ x: 1, y: -1 })
     .toMovements();
 
   const shiftLeaders = (i: number): DancerPosition =>
     DancerPosition.new({
-      x: 0,
-      y: 0.5 * i,
+      x: -0.5,
+      y: 1 * i,
       r: 0,
       angle: 0,
     });
 
   const shiftFollowers = (i: number): DancerPosition =>
     DancerPosition.new({
-      x: 0,
-      y: 0.5 * i,
+      x: +0.5,
+      y: 1 * i,
       r: 0,
       angle: 0,
     });
